ced3d46498f08e02abd1e295ba583229609ae990
axtap <axtap@mail.ru>
2025-05-06 17:45:05 +0500

    Add UserControllerTest

---
+++ src/test/java/ru/astondevs/service/user/controller/UserControllerTest.java
@@ -0,0 +1,53 @@
+package ru.astondevs.service.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.astondevs.service.user.dto.UserDto;
+import ru.astondevs.service.user.service.UserService;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void checkFindById() throws Exception {
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .name("Test")
+                .email("test@test.com")
+                .age(35)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        when(userService.findById(1L)).thenReturn(userDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userDto.getId()))
+                .andExpect(jsonPath("$.name").value(userDto.getName()))
+                .andExpect(jsonPath("$.email").value(userDto.getEmail()))
+                .andExpect(jsonPath("$.age").value(userDto.getAge()));
+    }
+
+
+}
